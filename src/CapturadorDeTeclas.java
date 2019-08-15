ab54f902259b16362a4e190494b5222db8fa21dd
Pietro Verdile <pietro.verdile@clickbus.com>
2019-08-15 11:14:24 -0300

    start game

--- src/CapturadorDeTeclas.java
+++ src/CapturadorDeTeclas.java
@@ -7,15 +7,25 @@ import javax.swing.JFrame;
 public class CapturadorDeTeclas {
 
 	public static void main(String[] args) {
+		System.out.println("Bunny");
+		System.out.println("PRESSIONE ENTER PARA COMEÇAR");
 		JFrame frame = new JFrame();
 		JButton button = new JButton();
 		frame.add(button);
 		frame.setVisible(true);
-		button.addKeyListener(new KeyAdapter(){
+
+		button.addKeyListener(
+				new KeyAdapter(){
 
 			@Override
 			public void keyPressed(KeyEvent e) {
-				System.out.println(e.getKeyCode());
+				if (e.getKeyCode() == 10) {
+					for (int i = 0; i < 8; i++) {
+						System.out.println("\n");
+					}
+					System.out.println("JOGO INICIADO");
+				}
+//				System.out.println(e.getKeyCode());
 			}
 		});
 	}
