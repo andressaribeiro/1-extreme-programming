ed0b160eaeb742116f3e71d34ace225beb002311
zefor2 <zefor2@gmail.com>
2019-08-15 11:34:29 -0300

    1

---
+++ src/curso/xp/service/Acoes.java
@@ -0,0 +1,12 @@
+package curso.xp.service;
+
+public interface Acoes {
+
+    public void sobe();
+    public void desce();
+    public void esquerda();
+    public void direita();
+    public void pulo();
+    public void tick();
+    public String tela();
+}
