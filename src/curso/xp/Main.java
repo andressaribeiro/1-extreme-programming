026109cdf9f03221fc1a36ff3e1e08586e74c849
Ana Albuquerque <albuquerque.apr@gmail.com>
2019-08-16 10:31:09 -0300

    flush

--- src/curso/xp/Main.java
+++ src/curso/xp/Main.java
@@ -33,6 +33,8 @@ public class Main {
         while (true) {
             Thread.sleep(1000);
             jogo.tick();
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
             System.out.println(jogo.tela());
         }
     }
